1b99eac88ce66ff2d05817c6dded52035636201d
Emilio <emilio2hd@gmail.com>
2022-02-05 11:29:49 +0000

    Add skeleton to server status cards

--- panel/static/javascript/server-status.js
+++ panel/static/javascript/server-status.js
@@ -3,16 +3,23 @@ Vue.component('server-status', {
   computed: {
     isOnline: function () {
       return this.status == 'online';
+    },
+    isOffline: function () {
+      return this.status == 'offline';
     }
   },
   template: `
-  <div v-if="isOnline">
-    <i class="fas fa-circle text-success"></i>
-    Online
-  </div>
-  <div v-else>
-    <i class="fas fa-circle text-danger"></i>
-    Offline
+  <div class="server-status">
+      <div v-if="isOnline">
+        <i class="fas fa-circle text-success"></i>
+        Online
+      </div>
+      <div v-else-if="isOffline">
+      </div>
+      <div v-else>
+        <i class="fas fa-circle text-secondary"></i>
+        Unknown
+      </div>
   </div>
   `
 })
