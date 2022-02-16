411f4642afee078689176161ff6b6106b8bf618a
Emilio <emilio2hd@gmail.com>
2022-02-16 08:45:39 +0000

    Add disk usage info

--- panel/utils/resources_functions.py
+++ panel/utils/resources_functions.py
@@ -1,6 +1,23 @@
 import re
 from rcon import Client
-from psutil import virtual_memory, cpu_percent
+from psutil import virtual_memory, cpu_percent, disk_usage
+
+
+def bytes2human(n):
+    # http://code.activestate.com/recipes/578019
+    # >>> bytes2human(10000)
+    # '9.8K'
+    # >>> bytes2human(100001221)
+    # '95.4M'
+    symbols = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
+    prefix = {}
+    for i, s in enumerate(symbols):
+        prefix[s] = 1 << (i + 1) * 10
+    for s in reversed(symbols):
+        if n >= prefix[s]:
+            value = float(n) / prefix[s]
+            return "%.1f%s" % (value, s)
+    return "%sB" % n
 
 
 def server_status(rcon_host, rcon_password):
@@ -16,8 +33,15 @@ def server_status(rcon_host, rcon_password):
 def server_resources():
     resources = dict()
 
+    disk = disk_usage('/')
+
     resources['ram'] = virtual_memory().percent
     resources['cpu'] = cpu_percent()
+    resources['disk'] = {
+        "free": bytes2human(disk.free),
+        "used": bytes2human(disk.used),
+        "usagePercent": disk.percent
+    }
 
     return resources
 
