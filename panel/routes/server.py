163874f7f4922068be7f218ea7625ba031b7e833
Emilio <emilio2hd@gmail.com>
2022-02-22 23:58:01 +0000

    refactoring server routes extracting some logic to a service file

--- panel/routes/server.py
+++ panel/routes/server.py
@@ -4,6 +4,7 @@ from os import path
 from flask import Blueprint, jsonify, current_app, request, Response, json
 from flask_login import login_required
 
+from ..services.server_options_service import read_config, save_config, prepared_config_to_view, formatted_config_lines
 from ..utils.power_actions import CommandStatus, actions as server_power_actions, is_zomboid_screen_session_up
 from ..utils.resources_functions import is_server_on, server_resources, online_players
 from .. import pz_server_state
@@ -61,21 +62,32 @@ def power_actions():
         return '', 500
 
 
-def read_config(pz_server_config: str):
-    with open(pz_server_config) as f:
-        config = dict(x.rstrip().split("=", 1) for x in f)
+def get_config(pz_server_config):
+    config = read_config(pz_server_config)
 
-    return config
+    return {
+        "WorkshopItems": prepared_config_to_view(config["WorkshopItems"]),
+        "Mods": prepared_config_to_view(config["Mods"])
+    }
 
 
 @server_blueprint.route('/server/options')
 @login_required
 def list_workshop_items():
-    config = read_config(current_app.config['PZ_SERVER_CONFIG'])
+    export_config = get_config(current_app.config['PZ_SERVER_CONFIG'])
 
-    return jsonify(
-        WorkshopItems=list(filter(None, config["WorkshopItems"].split(";"))),
-        Mods=list(filter(None, config["Mods"].split(";")))
+    return jsonify(export_config)
+
+
+@server_blueprint.route('/server/options/export')
+@login_required
+def export_server_config():
+    export_config = get_config(current_app.config['PZ_SERVER_CONFIG'])
+
+    return Response(
+        formatted_config_lines(export_config),
+        mimetype='text/event-stream',
+        headers={"Content-Disposition": "attachment;filename=server_config.ini"}
     )
 
 
@@ -84,44 +96,15 @@ def list_workshop_items():
 def save_items():
     request_data = request.get_json()
 
-    config = read_config(current_app.config['PZ_SERVER_CONFIG'])
-
-    if "WorkshopItems" in request_data:
-        workshop_items = request_data.get("WorkshopItems", [])
-        config['WorkshopItems'] = ";".join(list(dict.fromkeys(workshop_items)))
-
-    if "Mods" in request_data:
-        mods = request_data.get("Mods", [])
-        config['Mods'] = ";".join(list(dict.fromkeys(mods)))
-
-    with open(current_app.config['PZ_SERVER_CONFIG'], 'w') as f:
-        for key in config.keys():
-            f.write("%s=%s\n" % (key, config[key]))
-
-    return jsonify(
-        WorkshopItems=list(filter(None, config["WorkshopItems"].split(";"))),
-        Mods=list(filter(None, config["Mods"].split(";")))
-    )
-
-@server_blueprint.route('/server/options/export')
-@login_required
-def export_server_config():
-    config = read_config(current_app.config['PZ_SERVER_CONFIG'])
+    config = save_config(current_app.config['PZ_SERVER_CONFIG'], request_data)
 
     export_config = {
-        "WorkshopItems": config["WorkshopItems"],
-        "Mods": config["Mods"]
+        "WorkshopItems": prepared_config_to_view(config["WorkshopItems"]),
+        "Mods": prepared_config_to_view(config["Mods"])
     }
 
-    def generate(config):
-        for key in config.keys():
-            yield "%s=%s\n" % (key, config[key])
+    return jsonify(export_config)
 
-    return Response(
-        generate(export_config),
-        mimetype='text/event-stream',
-        headers={"Content-Disposition": "attachment;filename=server_config.ini"}
-    )
 
 @server_blueprint.route('/server/log')
 @login_required
---
+++ panel/services/server_options_service.py
@@ -0,0 +1,36 @@
+ALLOWED_CONFIG_OPTIONS = ["WorkshopItems", "Mods"]
+
+
+def prepared_config_to_save(config_value: dict):
+    return ";".join(list(dict.fromkeys(config_value)))
+
+
+def prepared_config_to_view(config_value):
+    return list(filter(None, config_value.split(";")))
+
+
+def formatted_config_lines(config: dict):
+    for key in config.keys():
+        yield "%s=%s\n" % (key, config[key])
+
+
+def read_config(pz_server_config: str):
+    with open(pz_server_config) as f:
+        config = dict(x.rstrip().split("=", 1) for x in f)
+
+    return config
+
+
+def save_config(pz_server_config: str, request_data: dict):
+    config = read_config(pz_server_config)
+
+    for config_key in ALLOWED_CONFIG_OPTIONS:
+        if config_key in request_data:
+            workshop_items = request_data.get(config_key, [])
+            config[config_key] = prepared_config_to_save(workshop_items)
+
+    with open(pz_server_config, 'w') as f:
+        for line in formatted_config_lines(config):
+            f.write(line)
+
+    return config
