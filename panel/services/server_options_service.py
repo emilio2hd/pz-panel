10cccbcb0dd13b31db6294e322594d52e5f1f2c6
Emilio <emilio2hd@gmail.com>
2022-02-28 13:46:48 +0000

    fix read and save of new config file

--- panel/services/server_options_service.py
+++ panel/services/server_options_service.py
@@ -1,3 +1,5 @@
+import re
+
 ALLOWED_CONFIG_OPTIONS = ["WorkshopItems", "Mods"]
 CONFIG_VALUE_SEPARATOR = ';'
 
@@ -14,24 +16,33 @@ def formatted_config_lines(config: dict):
     for key in config.keys():
         yield "%s=%s\n" % (key, config[key])
 
-
 def read_config(pz_server_config: str):
-    with open(pz_server_config) as f:
-        config = dict(x.rstrip().split("=", 1) for x in f)
+    config = dict()
+
+    with open(pz_server_config, 'r') as reader:
+        for line in reader.readlines():
+            if line.startswith('#'):
+                continue
+
+            split_line = line.strip().split("=", 1)
+            if len(split_line) > 1:
+                config.update([split_line])
 
     return config
 
 
 def save_config(pz_server_config: str, request_data: dict):
-    config = read_config(pz_server_config)
+    config_content = ''
+    with open(pz_server_config, 'r') as reader:
+        config_content = reader.read()
 
     for config_key in ALLOWED_CONFIG_OPTIONS:
         if config_key in request_data:
-            workshop_items = request_data.get(config_key, [])
-            config[config_key] = prepared_config_to_save(workshop_items)
+            items = request_data.get(config_key, [])
+            config_content = re.sub(rf"{config_key}=(.*)", "%s=%s" % (config_key, prepared_config_to_save(items)), config_content)
 
     with open(pz_server_config, 'w') as f:
-        for line in formatted_config_lines(config):
-            f.write(line)
+        f.write(config_content)
 
+    config = read_config(pz_server_config)
     return config
--- panel/static/javascript/list-items.js
+++ panel/static/javascript/list-items.js
@@ -14,39 +14,48 @@ Vue.component('list-items', {
       if(item.checked) {
         return 'warning';
       }
-    }
+    },
+    hasItems() {
+      return this.items.length > 0;
+    },
   },
   template: `
-  <div class="list-items">
-  <b-skeleton-wrapper :loading="loading" class="row">
-    <template #loading>
-      <b-skeleton-table
-        hide-header
-        :rows="10"
-        :columns="1"
-        :table-props="{ bordered: true, striped: true }"
-      ></b-skeleton-table>
-    </template>
-    <b-table-simple striped hover>
-      <b-tbody>
-        <b-tr
-          v-for="item in items"
-          :key="item.name"
-          :variant="applySelectionIfChecked(item)"
-          role="button"
-          @click="item.checked = !item.checked"
-        >
-          <b-td width="3%">
-            <input type="checkbox"
-              v-model="item.checked"
-              :id="'item_' + item.name"
-            />
-          </b-td>
-          <b-td>{{item.name}}</b-td>
-        </b-tr>
-      </b-tbody>
-    </b-table-simple>
-  </b-skeleton-wrapper>
+  <div class="list-items h-100">
+    <b-skeleton-wrapper :loading="loading" class="row">
+      <template #loading>
+        <b-skeleton-table
+          hide-header
+          :rows="10"
+          :columns="1"
+          :table-props="{ bordered: true, striped: true }"
+        ></b-skeleton-table>
+      </template>
+      <div
+        v-if="!hasItems()"
+        class="d-flex align-items-center justify-content-center h-100"
+      >
+        <div class="text-muted">No items to display</div>
+      </div>
+      <b-table-simple striped hover v-else>
+        <b-tbody>
+          <b-tr
+            v-for="item in items"
+            :key="item.name"
+            :variant="applySelectionIfChecked(item)"
+            role="button"
+            @click="item.checked = !item.checked"
+          >
+            <b-td width="3%">
+              <input type="checkbox"
+                v-model="item.checked"
+                :id="'item_' + item.name"
+              />
+            </b-td>
+            <b-td>{{item.name}}</b-td>
+          </b-tr>
+        </b-tbody>
+      </b-table-simple>
+    </b-skeleton-wrapper>
   </div>
   `
 });
