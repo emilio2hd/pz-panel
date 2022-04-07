6b53f465b2c041e963e2b75e48b1612549ad6fea
Emilio <emilio2hd@gmail.com>
2022-04-07 18:38:25 +0100

    Load .env file if dotenv is installed

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,15 @@
 
 from os import environ, path
-from dotenv import load_dotenv
 
 from panel import create_app
 
-basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, ".env"))
+try:
+    from dotenv import load_dotenv
+    basedir = path.abspath(path.dirname(__file__))
+    load_dotenv(path.join(basedir, ".env"))
+except ImportError:
+    pass
+
 
 app = create_app(environ.get("FLASK_ENV", "development"))
 
